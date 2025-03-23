a33c1dfec7ee377dfa5d8e0d20f63d7eb704f8d0
marcaidev16 <marcbauteleco@gmail.com>
2025-03-23 19:18:47 +0100

    interview generation

---
+++ app/(root)/interview/page.tsx
@@ -0,0 +1,22 @@
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const Page = async () => {
+  const user = await getCurrentUser();
+
+  return (
+    <>
+      <h3>Interview generation</h3>
+
+      <Agent
+        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+        userName={user?.name!}
+        userId={user?.id}
+        //profileImage={user?.profileURL}
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
